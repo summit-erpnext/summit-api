9d5b3e292db8040c55d965deb89b5ec571ea6b9f
manoj8848 <manoj@8848digital.com>
2024-07-11 16:21:44 +0530

    fix: changed fields name in get list product limit

--- summitapp/api/v2/utils.py
+++ summitapp/api/v2/utils.py
@@ -454,8 +454,8 @@ def get_list_product_limit(user_role, customer_id):
     elif customer_id:
         grp = frappe.db.get_value("Customer", customer_id, 'customer_group')
         if grp:
-            customer_group_limit = frappe.db.get_value("Customer Group", grp, "product_limit")
-            apply_customer_group_limit = frappe.db.get_value("Customer Group", grp, "apply_product_limit")
+            customer_group_limit = frappe.db.get_value("Customer Group", grp, "set_product_limit")
+            apply_customer_group_limit = frappe.db.get_value("Customer Group", grp, "apply_the_product_limit")
             
             if customer_group_limit is not None and apply_customer_group_limit == 1:
                 return customer_group_limit
