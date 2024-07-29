2bb174f62f49a8d30ba6d673a5f4a0fdad0ab8af
manoj8848 <manoj@8848digital.com>
2024-07-29 19:19:37 +0530

    feat: added cache control in banner api

--- summitapp/api/v2/banner.py
+++ summitapp/api/v2/banner.py
@@ -1,6 +1,10 @@
 import frappe
 from summitapp.utils import success_response, error_response
 from summitapp.api.v2.utils import get_field_names,get_logged_user
+import hashlib
+import datetime
+import json
+from werkzeug.wrappers import Response
 
 # def get(kwargs):
 #     try:
@@ -78,28 +82,85 @@ def button_info(banner):
     return btn_list
 
 
+# def get(kwargs):
+#     try:
+#         fields = get_field_names("Banner")
+
+#         banners = frappe.get_list("Home Banner", filters={"show_on_home_page": 1}, fields=["*"],order_by ="sequence")
+
+#         if kwargs.get("category"):
+#             banners = frappe.get_list("Home Banner", filters={"category": kwargs.get("category")}, fields=["*"])
+
+#         for banner in banners:
+#             banner['btn_info'] = button_info(banner)
+
+#         # Extracting desired fields after adding button_info
+#         for banner in banners:
+#             filtered_banner = {key: value for key, value in banner.items() if key in fields}
+#             banner = {}
+#             banner.update(filtered_banner)
+
+#         return success_response(banners)
+        
+#     except Exception as e:
+#         frappe.logger("banner").exception(e)
+#         return error_response(e)
+
+    
 def get(kwargs):
     try:
         fields = get_field_names("Banner")
-
         banners = frappe.get_list("Home Banner", filters={"show_on_home_page": 1}, fields=["*"],order_by ="sequence")
-
         if kwargs.get("category"):
             banners = frappe.get_list("Home Banner", filters={"category": kwargs.get("category")}, fields=["*"])
-
         for banner in banners:
             banner['btn_info'] = button_info(banner)
-
         # Extracting desired fields after adding button_info
         for banner in banners:
             filtered_banner = {key: value for key, value in banner.items() if key in fields}
             banner = {}
             banner.update(filtered_banner)
 
-        return success_response(banners)
+        response_body = frappe.as_json(banners)
         
+        etag = hashlib.md5(response_body.encode()).hexdigest()
+        if etag:
+            item = etag[0] if etag else None
+            headers = {}
+            if item:
+                headers["Etag"] = etag
+
+        if frappe.db.exists("Etag",etag):
+            data =  {"status" : "Success","data":banners}
+            return custom_response(data, headers=headers, status_code=304)
+        else:
+            etab = frappe.get_doc({
+            "doctype": 'Etag',
+            'etag': etag,
+            })
+            etab.insert(ignore_permissions=True)
+            etab.save()
+            print("Etag created succesfull",etab)
+            data =  {"status" : "Success","data":banners}
+            return custom_response(data, headers=headers)
+       
     except Exception as e:
         frappe.logger("banner").exception(e)
         return error_response(e)
+        
+def json_handler(obj):
+    """Custom JSON handler for objects not serializable by default json code."""
+    if isinstance(obj, (datetime.date, datetime.datetime)):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
 
-
+def custom_response(data, headers=None, status_code=200):
+    response = Response()
+    response.status_code = status_code
+    response.mimetype = "application/json"
+    response.data = json.dumps(data, default=json_handler, separators=(",", ":"))
+    if headers:
+        for key, value in headers.items():
+            response.headers[key] = value
+    response.headers["Cache-Control"] = "no-cache, max-age=120"
+    return response
