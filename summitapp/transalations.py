79689b12b02a08ca79b2c1f2f1a861d4befc440a
manoj8848 <manoj@8848digital.com>
2024-11-12 12:45:54 +0530

    feat: renamed doctype Tag to Feature Collection

---
+++ summitapp/transalations.py
@@ -0,0 +1,18 @@
+import frappe
+
+def create_translations():
+    print("create_translations")
+    translations = [
+        {"source": "Tag", "translated": "Featured Collection"},
+    ]
+    for translation in translations:
+        create_translation_if_not_exists(translation["source"], translation["translated"])
+
+def create_translation_if_not_exists(source_text, translated_text):
+    if not frappe.db.exists("Translation", {"source_text": source_text}):
+        translation_doc = frappe.new_doc("Translation")
+        translation_doc.source_text = source_text
+        translation_doc.translated_text = translated_text
+        translation_doc.save(ignore_permissions=True)
+
+
