37ab49a58f165fb0a5e91c9ac1eaf77d5c030d5f
aartimanjare <aartimanjare@gmail.com>
2025-04-11 17:04:43 +0530

    fix: removed customer js and custom fields

---
+++ summitapp/summitapp/doctype/vehicle_variant_name/vehicle_variant_name.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, 8848 Digital LLP and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Vehicle Variant Name", {
+// 	refresh(frm) {
+
+// 	},
+// });
