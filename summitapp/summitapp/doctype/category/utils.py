3413effe71d2998b8fc242449c69aaf24015717d
sameer bhoir <sameer@8848@gmail.com>
2025-10-10 11:49:04 +0530

    feat: update item details code

--- summitapp/summitapp/doctype/category/utils.py
+++ summitapp/summitapp/doctype/category/utils.py
@@ -1,8 +1,15 @@
 import frappe
 
 def set_item_fields(doc):
+    item_meta = frappe.get_meta("Item")
+
+    item_field_map = {
+        df.label: df.fieldname
+        for df in item_meta.fields
+        if df.label
+    }
+
     for item_field in doc.item_details:
-        item_field.fieldname = frappe.db.get_value(
-            "Item Field Details",item_field.label,"fieldname"
-        )
-    
+        label = item_field.label
+        if label in item_field_map:
+            item_field.fieldname = item_field_map[label]
