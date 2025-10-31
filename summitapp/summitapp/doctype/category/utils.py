0361e7dc305bbeec9da672b0e1302ae0d26e6983
sameer bhoir <sameer@8848@gmail.com>
2025-10-31 10:11:59 +0530

    feat: update item detail page api

---
+++ summitapp/summitapp/doctype/category/utils.py
@@ -0,0 +1,17 @@
+import frappe
+
+def set_item_fields(doc):
+    item_meta = frappe.get_meta("Item")
+
+    item_field_map = {
+        df.label: df.fieldname
+        for df in item_meta.fields
+        if df.label
+    }
+
+    for item_field in doc.item_details:
+        label = item_field.label
+        if label in item_field_map:
+            item_field.fieldname = item_field_map[label]
+        else:
+            item_field.fieldname = None
